dcf35e919d9cf14ba3e426c11b8a6917a5b53af4
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-01-28 01:07:41 +0500

    origin

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+const header = document.querySelector('header') 
+const academiesDd = document.querySelector('.header-dropdown-btn-academies')
+const servicesDd =document.querySelector('.header-dropdown-btn-services')
+academiesDd.addEventListener('mouseenter',()=>  header.classList.add('active'))
+academiesDd.addEventListener('mouseleave',()=>  header.classList.contains('fixed') || header.classList.remove('active'))
+servicesDd.addEventListener('mouseenter',()=> header.classList.add('active'))
+servicesDd.addEventListener('mouseleave',()=>header.classList.contains('fixed') || header.classList.remove('active'))
+
+window.onscroll = function(e) {
+    // print "false" if direction is down and "true" if up
+    console.log();
+    if(this.oldScroll > 150){
+        if(this.oldScroll > this.scrollY){
+            header.classList.add('active')
+            header.classList.add('fixed')
+        }else{
+            header.classList.remove('active')
+            header.classList.remove('fixed')
+
+        }
+    }else{
+        header.classList.remove('active')
+        header.classList.remove('fixed')
+    }
+    this.oldScroll = this.scrollY;
+  }
