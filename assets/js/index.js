b292044419d41d9cb97e84f98c75b103dd24649c
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-01-28 17:33:23 +0500

    origin

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,92 @@
 const header = document.querySelector('header') 
 const academiesDd = document.querySelector('.header-dropdown-btn-academies')
-const servicesDd =document.querySelector('.header-dropdown-btn-services')
-const video =document.querySelector('video')
+const servicesDd =document.querySelector('.header-dropdown-btn-services') 
 
 
 
 
-academiesDd.addEventListener('mouseenter',()=>  header.classList.add('active'))
-academiesDd.addEventListener('mouseleave',()=>  header.classList.contains('fixed') || header.classList.remove('active'))
-servicesDd.addEventListener('mouseenter',()=> header.classList.add('active'))
-servicesDd.addEventListener('mouseleave',()=>header.classList.contains('fixed') || header.classList.remove('active'))
+academiesDd.addEventListener('mouseenter',()=>  {
+    header.querySelector('.bunyanLogo').src ='./assets/images/bunyanLogoBlue.svg'
+    header.classList.add('active')
+    header.querySelector('.SearchIcon').src ='./assets/images/home/icons/blueSearch.svg'
+    header.querySelectorAll('.Arrow').forEach(item=> item.src = './assets/images/home/icons/blueArrow.svg')
+    academiesDd.querySelector('.Arrow').src='/assets/images/home/icons/primaryArrow.svg'
+})
+academiesDd.addEventListener('mouseleave',()=>  {
+    header.classList.contains('fixed') || header.classList.remove('active')
+    header.querySelector('.SearchIcon').src ='./assets/images/home/icons/search.svg'
+    header.querySelectorAll('.Arrow').forEach(item=> item.src = './assets/images/home/icons/arrow.svg')
+    header.querySelector('.bunyanLogo').src ='./assets/images/bunyanLogo.svg'
+})
+servicesDd.addEventListener('mouseenter',()=> {
+    header.querySelector('.bunyanLogo').src ='./assets/images/bunyanLogoBlue.svg'
+    header.classList.add('active')
+    header.querySelector('.SearchIcon').src ='./assets/images/home/icons/blueSearch.svg'
+    header.querySelectorAll('.Arrow').forEach(item=> item.src = './assets/images/home/icons/blueArrow.svg')
+    servicesDd.querySelector('.Arrow').src='/assets/images/home/icons/primaryArrow.svg'
+})
+servicesDd.addEventListener('mouseleave',()=>{
+    header.classList.contains('fixed') || header.classList.remove('active')
+    header.querySelector('.SearchIcon').src ='./assets/images/home/icons/search.svg'
+    header.querySelectorAll('.Arrow').forEach(item=> item.src = './assets/images/home/icons/arrow.svg')
+    header.querySelector('.bunyanLogo').src ='./assets/images/bunyanLogo.svg'
+})
 
+let a=1
 
 window.onscroll = function(e) {
-    // print "false" if direction is down and "true" if up
-    console.log();
-    if(this.oldScroll > 150){
-        if(this.oldScroll > this.scrollY){
-            header.classList.add('active')
+     let height = window.innerHeight
+     if(height * 1.3  > this.oldScroll){
+        letsAnimate()
+     }
+    if(this.oldScroll > height){
+          header.classList.add('active')
             header.classList.add('fixed')
-        }else{
-            header.classList.remove('active')
-            header.classList.remove('fixed')
+            header.querySelector('.SearchIcon').src ='./assets/images/home/icons/blueSearch.svg'
+            header.querySelectorAll('.Arrow').forEach(item=> item.src = './assets/images/home/icons/blueArrow.svg')
+            header.querySelector('.bunyanLogo').src ='./assets/images/bunyanLogoBlue.svg'
+         
 
-        }
+        
     }else{
         header.classList.remove('active')
         header.classList.remove('fixed')
+        header.querySelector('.SearchIcon').src ='./assets/images/home/icons/search.svg'
+        header.querySelectorAll('.Arrow').forEach(item=> item.src = './assets/images/home/icons/arrow.svg')
+        header.querySelector('.bunyanLogo').src ='./assets/images/bunyanLogo.svg'
     }
     this.oldScroll = this.scrollY;
   }
-
-  setTimeout(()=>{
-       
-      console.log(video)
-    function playVid() {
-        video.play();
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10, 
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
     }
-    playVid()
-    
-  },50)
+})
+
+
+const letsAnimate=()=>{
+    if(a===1){
+    $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });}
+    a = 2
+ }
